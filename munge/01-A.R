3e3a0581569845dab2f156ae55aa1d9574e02f7a
Reiners, Paul <Paul.Reiners@Pearson.com>
2014-04-15 17:06:19 -0500

    More munging.

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,5 +1,14 @@
 # Preprocessing script.
 
+# Convert Income to a numeric
+incomeLevels = 
+  c("", "under $25,000", "$25,001 - $50,000", "$50,000 - $74,999", 
+    "$75,000 - $100,000", "$100,001 - $150,000", "over $150,000")
+allTrain$Income = factor(allTrain$Income,levels=incomeLevels,ordered=TRUE)
+allTrain$Income = as.numeric(allTrain$Income)
+test$Income = factor(test$Income,levels=incomeLevels,ordered=TRUE)
+test$Income = as.numeric(test$Income)
+
 # Randomly split the data into training and testing sets
 set.seed(1000)
 split = sample.split(allTrain$Happy, SplitRatio = 0.65)
