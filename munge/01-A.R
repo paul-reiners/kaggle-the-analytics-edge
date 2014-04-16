d83b55a6183fed859f0ded838cc76a57e0d6b779
Reiners, Paul <Paul.Reiners@Pearson.com>
2014-04-16 09:59:25 -0500

    Formatting.

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,21 +1,26 @@
 # Preprocessing script.
 
-# Convert Income to a numeric
+## Convert Income to a numeric
 incomeLevels = 
   c("", "under $25,000", "$25,001 - $50,000", "$50,000 - $74,999", 
     "$75,000 - $100,000", "$100,001 - $150,000", "over $150,000")
+
 allTrain$Income = factor(allTrain$Income,levels=incomeLevels,ordered=TRUE)
 allTrain$Income = as.numeric(allTrain$Income)
+
 test$Income = factor(test$Income,levels=incomeLevels,ordered=TRUE)
 test$Income = as.numeric(test$Income)
 
-# Convert EducationLevel to a numeric
+## Convert EducationLevel to a numeric
 educationLevels = 
   c("", "Current K-12", "High School Diploma", "Current Undergraduate", 
-    "Associate's Degree", "Bachelor's Degree", "Master's Degree", "Doctoral Degree")
+    "Associate's Degree", "Bachelor's Degree", "Master's Degree", 
+    "Doctoral Degree")
+
 allTrain$EducationLevel = 
   factor(allTrain$EducationLevel,levels=educationLevels,ordered=TRUE)
 allTrain$EducationLevel = as.numeric(allTrain$EducationLevel)
+
 test$EducationLevel = factor(test$EducationLevel,levels=educationLevels,ordered=TRUE)
 test$EducationLevel = as.numeric(test$EducationLevel)
 
