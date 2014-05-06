9417305950e581d9775ad3be68dbdc5fa78cbcaf
Paul Reiners <paul.reiners@gmail.com>
2014-05-05 20:54:46 -0500

    Finishing up.

--- src/RandomForest.R
+++ src/RandomForest.R
@@ -1,29 +1,32 @@
-## RANDOM FOREST 
-# Current AUC = 0.7365237/xxx 
+## RANDOM FOREST
 
-setwd("~/Dropbox/education/EdX/MITx/15.071x/kaggle-the-analytics-edge") 
-library('ProjectTemplate') 
-load.project() 
+# BEST
+# Private Score = 0.77280
+# Public Score  = 0.74020
 
-# Convert outcome to factor 
-trainTrain$Happy = as.factor(trainTrain$Happy) 
-trainTest$Happy = as.factor(trainTest$Happy) 
+setwd("~/Dropbox/education/EdX/MITx/15.071x/kaggle-the-analytics-edge")
+library('ProjectTemplate')
+load.project()
 
-# Build random forest model 
-forest = randomForest(Happy ~ . - UserID, data=trainTrain, nodesize=25) 
+# Convert outcome to factor
+trainTrain$Happy = as.factor(trainTrain$Happy)
+trainTest$Happy = as.factor(trainTest$Happy)
 
-# Make predictions 
-PredictForest = predict(forest, newdata = trainTest,type="prob")[,2] 
+# Build random forest model
+forest = randomForest(Happy ~ . - UserID, data=trainTrain, nodesize=25)
+
+# Make predictions
+PredictForest = predict(forest, newdata = trainTest,type="prob")[,2]
 
 # Test set AUC 
-auc1 <- performance(prediction(PredictForest,trainTest$Happy),"auc")@y.values[[1]] 
-auc1 
+auc1 <- performance(prediction(PredictForest,trainTest$Happy),"auc")@y.values[[1]]
+auc1
 
-# Create model on all data. 
-train$Happy = as.factor(train$Happy) 
-submissionModel = randomForest(Happy ~ . - UserID, data=train, nodesize=25) 
+# Create model on all data.
+train$Happy = as.factor(train$Happy)
+submissionModel = randomForest(Happy ~ . - UserID, data=train, nodesize=25)
 
-# Make submission 
-testPred = predict(submissionModel, newdata=test,type="prob")[,2] 
-submission = data.frame(UserID = test$UserID, Probability1 = testPred) 
+# Make submission
+testPred = predict(submissionModel, newdata=test,type="prob")[,2]
+submission = data.frame(UserID = test$UserID, Probability1 = testPred)
 write.csv(submission, "./reports/RandomForest.csv", row.names=FALSE)
